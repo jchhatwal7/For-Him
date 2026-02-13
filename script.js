0a58c0b676a8e0dc0f842f627b3a9ad69d1588f2
jchhatwal7 <jchhatwal7@gmail.com>
2026-02-13 17:13:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ document.querySelectorAll(".letter").forEach(letter => {
     letter.classList.toggle("open");
   });
 });
+
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+  heart.innerText = "🤍";
+
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.fontSize = Math.random() * 10 + 18 + "px";
+
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 4000);
+}
+
+setInterval(createHeart, 1500);
